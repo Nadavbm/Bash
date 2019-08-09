92c5a79a5dc589e3a05f3623a045c869db3d35c4
Nadav Ben Mazia <nadav.benmazia@aeriagames.com>
2019-08-09 16:27:23 +0200

    Cheatsheet update

--- CheatSheet.sh
+++ CheatSheet.sh
@@ -9,24 +9,59 @@
 # Variables and data types:
 STR="Sting variable"
 INT=2
-# Parameters to bash scripts within the script:
-PARAM1="$1" # First parameter from terminal: $ script.sh firstParam
-PARAM2="$2" # Second parameter from terminal: $ script.sh firstParam secondParam
-# Useful terminal output variables:
-DATE="$( date +%F )" # Date in form 2018-12-17
-DATE_NOW="$(date +%Y/%m/%dT%H:%M:%S)" # to seconds
+#
+# Variables from stdout
+DATE="$( date +%F )"                    # Date in form 2018-12-17
+DATE_NOW="$(date +%Y/%m/%dT%H:%M:%S)"   # to seconds
 YESTERDAY="$( date -d 'yesterday 00:00:00' '+%s%3N' )"
-HOST="$( hostname )" # VM hostname
-DIR="/home/user" # Directory location
-TEXT="${DIR}/List.txt" #TXT file contain a list
+HOST="$( hostname )"                    # hostname
+DIR="/home/user"                        # directory location
+TEXT="${DIR}/List.txt"                  # txt file contain a list
 LIST=$( cut -d' ' -f5 ${DIR}/${TEXT} | paste -d' ' -s )
-ARRAY=( ${LIST} )   # Set an array from the list above
+ARRAY=( ${LIST} )                       # set an array from the list abov
+#
 # Echo variables:
 echo "String"
 echo 1
 echo "$( date +%F )"
 echo "[`date +%F--%H-%M`]"
-echo $DATE # The DATE variable set above
+echo $DATE                              # The DATE variable set above
+#
+# Colorize echos
+echo -e "\e[92mToday is the $DATE"
+echo -e "\e[94mThe directory you are looking for: \n$DIR"
+echo -e "\e[97mThe elements in this array are: \n ${ARRAY[@]}"
+# Special variables
+Arg1="$1"                               # First parameter from terminal: $ script.sh firstParam
+Arg2="$2"                               # Second parameter from terminal: $ script.sh firstParam secondParam
+NumOfArguments="$#"                     # Special variable that show the number of arguments that passed to the script
+ExitStatusOfLastCommand="$?"            # expands to the exit status of the most recently executed foreground pipeline.
+PARAM4="$*" 
+PARAM3="$!"            #   
+#
+# Conditional programming
+if [[ condition ]]; then
+  # statement
+elif [[ condition ]]; then
+  # statement
+else
+  # statement
+fi
+# If statement:
+if [ ! -f $1 ]; then
+    echo "$1 -- No such File!"
+fi
+# Another if statement:
+/usr/bin/command
+if [[ $? -gt 0 ]];then
+ echo "Operation failed at [`date +%F_%H:%M:%S`]"
+ exit 1
+fi
+echo "Operation succeed!"
+exit 0
+# If then else statement:
+
+
 # Functions:
 
 function helloWorld () {
@@ -42,43 +77,16 @@ if [[ ${DATE} == "12-12-2019" ]]; then
   echo "Today is the 12th of Dec 2019"
 fi
 
-# Error handling
-if [[ $? -gt 0 ]];then
- echo "Operation failed at [`date +%F_%H:%M:%S`]"
- exit 1
-fi
-
-# if else statements:
-if [[ $? -gt 0 ]]; then
-  echo "We have an error"
-  exit 1 
-else
-  echo "We don't have an error"
-  exit 0
-fi
-
-# If, elif ele statements:
-if [[ condition ]]; then
-  #statements
-elif [[ condition ]]; then
-  #statements
-else
-
-fi
-
-# Statements examples:
-PACKAGE="$( rpm -qa | grep wget )" # Check if wget installed
-
-if [ -z "$PACKAGE"  ]]; then
-  yum install -y wget # If null - install wget
-fi
-
-
-
-# Arrays:
-
-
 # Loops:
+echo "Count until 10"
 for (( i = 0; i < 10; i++ )); do
-  #statements
+    echo $i
+done
+
+# The $@ will create an arrays from the parameters added to the script execution in terminal
+> bash myArrayParams param1 param2 param2
+#!/bin/bash
+for i in "$@"
+do
+    echo "$i"
 done
